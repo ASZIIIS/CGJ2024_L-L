eb2ced23c5cd9a37de149de931b9a9383d15b7b3
ASZIIIS-LAPTOP\ASZIIIS <asziiis@163.com>
2024-07-06 18:37:59 +0800

    地图边缘跨越机制

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridManager
+{
+    public int size;
+    private static Vector3[] halfUnitVector;
+    public static void initHalfUnitVector(){
+        halfUnitVector=new Vector3[6];
+        Grid grid=GameObject.Find("Grid").GetComponent<Grid>();
+        Vector3 centerToWorld=grid.CellToWorld(new Vector3Int(0,0,-10));
+        Vector3 nextToWorld;
+        nextToWorld=grid.CellToWorld(new Vector3Int(1,0,-10));//Up
+        halfUnitVector[0]=(nextToWorld-centerToWorld)/2;
+        nextToWorld=grid.CellToWorld(new Vector3Int(0,1,-10));//RightUp
+        halfUnitVector[1]=(nextToWorld-centerToWorld)/2;
+        nextToWorld=grid.CellToWorld(new Vector3Int(-1,1,-10));//RightDown
+        halfUnitVector[2]=(nextToWorld-centerToWorld)/2;
+        nextToWorld=grid.CellToWorld(new Vector3Int(-1,0,-10));//Down
+        halfUnitVector[3]=(nextToWorld-centerToWorld)/2;
+        nextToWorld=grid.CellToWorld(new Vector3Int(-1,-1,-10));//LeftDown
+        halfUnitVector[4]=(nextToWorld-centerToWorld)/2;
+        nextToWorld=grid.CellToWorld(new Vector3Int(0,-1,-10));//LeftUp
+        halfUnitVector[5]=(nextToWorld-centerToWorld)/2;
+    }
+    public GridManager(int size){
+        this.size=size;
+        GridManager.initHalfUnitVector();
+    }
+    public Vector3Int move(Vector3Int origin, int direction){
+        int x=origin.x;
+        int y=origin.y;
+        int targetX=0, targetY=0;
+        bool outBound=false;
+        switch(direction){
+            case 0://Up
+                if(x+1+(Mathf.Abs(y)+1)/2>size-1){
+                    targetX=-x-(Mathf.Abs(y)%2);
+                    targetY=y;
+                    outBound=true;
+                }else{
+                    targetX=x+1;
+                    targetY=y;
+                }
+                break;
+            case 1://RightUp
+                if(y+1>size-1){
+                    targetY=-(size/2)-x;
+                    targetX=1-size-(targetY)/2;
+                    outBound=true;
+                }else if(x+1+(y+1)/2>size-1){
+                    targetX=(size-1)/2-y;
+                    targetY=1-size;
+                    outBound=true;
+                }else{
+                    targetX=x+(Mathf.Abs(y)%2);
+                    targetY=y+1;
+                }
+                break;
+            case 2://RightDown
+                if(y+1>size-1){
+                    targetY=x-(size-1)/2;
+                    targetX=size-1+(targetY-1)/2;
+                    outBound=true;
+                }else if(-x+1+(y)/2>size-1&&y>=0){
+                    targetX=-size/2+y;
+                    targetY=1-size;
+                    outBound=true;
+                }else{
+                    targetX=x-((Mathf.Abs(y)+1)%2);
+                    targetY=y+1;
+                }
+                break;
+            case 3://Down
+                if(-x+1+(Mathf.Abs(y))/2>size-1){
+                    targetX=-x-(Mathf.Abs(y)%2);
+                    targetY=y;
+                    outBound=true;
+                }else{
+                    targetX=x-1;
+                    targetY=y;
+                }
+                break;
+            case 4://LeftDown
+                if(-y+1>size-1){
+                    targetY=(size-1)/2-x;
+                    targetX=size-1-(targetY+1)/2;
+                    outBound=true;
+                }else if(-x+1-(y)/2>size-1&&y<=0){
+                    targetX=-(size)/2-y;
+                    targetY=size-1;
+                    outBound=true;
+                }else{
+                    targetX=x-(Mathf.Abs(y+1)%2);
+                    targetY=y-1;
+                }
+                break;
+            case 5://LeftUp
+                if(-y+1>size-1){
+                    targetY=x+(size)/2;
+                    targetX=(targetY)/2+1-size;
+                    outBound=true;
+                }else if(x+1+(-y+1)/2>size-1){
+                    targetX=(size-1)/2+y;
+                    targetY=size-1;
+                    outBound=true;
+                }else{
+                    targetX=x+(Mathf.Abs(y)%2);
+                    targetY=y-1;
+                }
+                break;
+        }
+        if(outBound){
+            return new Vector3Int(targetX, targetY, -8);
+        }else{
+            return new Vector3Int(targetX, targetY, -9);
+        }
+    }
+    
+}
--- Assets/Scripts/TestHex.cs
+++ Assets/Scripts/TestHex.cs
@@ -6,10 +6,17 @@ using UnityEngine.Tilemaps;
 public class TestHex : MonoBehaviour
 {
     private Grid grid;
+    private GridManager manager;
+    GameObject head, body;
+    int direction;
     // Start is called before the first frame update
     void Start()
     {
         grid=GetComponent<Grid>();
+        // manager=new GridManager(6);
+        // head=GameObject.Find("SnakeHead");
+        // body=GameObject.Find("SnakeBody");
+        // direction=0;
     }
 
     // Update is called once per frame
@@ -18,8 +25,34 @@ public class TestHex : MonoBehaviour
         if(Input.GetMouseButtonDown(0)){
             Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3Int gridPosition=grid.WorldToCell(clickPosition);
-            Vector3 centerPosition=grid.CellToWorld(gridPosition);
-            Debug.Log(clickPosition.ToString()+"->"+gridPosition.ToString()+"->"+centerPosition.ToString());
+            // Vector3Int targetPosition=manager.move(gridPosition, direction);
+            // head.transform.position=grid.CellToWorld(targetPosition);
+            // body.transform.position=grid.CellToWorld(gridPosition+new Vector3Int(0,0,1));
+            // Debug.Log(gridPosition.ToString()+"->"+targetPosition.ToString());
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha0)){
+            direction=0;
+            Debug.Log("Direction: "+direction);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            direction=1;
+            Debug.Log("Direction: "+direction);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2)){
+            direction=2;
+            Debug.Log("Direction: "+direction);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha3)){
+            direction=3;
+            Debug.Log("Direction: "+direction);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha4)){
+            direction=4;
+            Debug.Log("Direction: "+direction);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha5)){
+            direction=5;
+            Debug.Log("Direction: "+direction);
         }
     }
 }
